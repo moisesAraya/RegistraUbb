8d31c178b458067ad8cf5bcdeb68cf60c19392aa
4ndreams <andrea.tapia2101@alumnos.ubiobio.cl>
2025-09-04 23:17:14 -0400

    docker y bd

--- lector/modlector/models.py
+++ lector/modlector/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Cargo(models.Model):
+    nombre_cargo = models.CharField(max_length=50)
+
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    horas_atrabajar = models.FloatField()
+    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE, default=1)
