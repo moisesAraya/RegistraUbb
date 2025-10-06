273654cc7e0a4f166bb7e64875a1d9aabd3bd4c6
root <root@localhost>
2025-10-06 01:44:13 +0000

    fix start

--- lector/start_gunicorn.sh
+++ lector/start_gunicorn.sh
@@ -2,7 +2,7 @@
 cd /root/RegistraUbb/lector
 source venv/bin/activate
 exec gunicorn mysite.wsgi:application \
-  --bind 0.0.0.0:1778 \
+  --bind 0.0.0.0:8080 \
   --workers 3 \
   --env DJANGO_SETTINGS_MODULE=mysite.production_settings \
   --access-logfile - \
