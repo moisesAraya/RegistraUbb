9443f8f90ff42c5fcfeb50bff4577db246e1992b
4ndreams <andrea.tapia2101@alumnos.ubiobio.cl>
2025-10-17 18:26:15 -0300

    fix debug

--- lector/mysite/settings.py
+++ lector/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g-xo9*6zdqi0+39a!!*(kb$=$+d*bmt@grp1n4*b_h)#6z9c1q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -104,7 +104,7 @@ else:  # PRODUCCIÓN - Base de datos remota
         }
     }
     # URL del backend para producción
-    BACKEND_URL = "http://146.83.194.142:1772"
+    BACKEND_URL = "http://146.83.194.142:1772/api/"
 
 
 # Password validation
