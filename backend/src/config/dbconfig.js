6c48a2396e538378054e48a5a554c56dde34e673
4ndreams <andrea.tapia2101@alumnos.ubiobio.cl>
2025-11-03 16:03:57 -0300

    fix index

--- backend/src/config/dbconfig.js
+++ backend/src/config/dbconfig.js
@@ -11,7 +11,7 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-    logging: false   
+    logging: false,   
   }
 );
 
