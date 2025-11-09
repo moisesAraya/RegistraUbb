60c7d956fada89b45caddf819ddb184a1732c7ba
root <root@localhost>
2025-11-09 23:23:23 +0000

    fix csrf settings

--- lector/mysite/settings.py
+++ lector/mysite/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g-xo9*6zdqi0+39a!!*(kb$=$+d*bmt@grp1n4*b_h)#6z9c1q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://146.83.194.142',
+    'https://146.83.194.142:1779',
+]
 
 
 # Application definition
