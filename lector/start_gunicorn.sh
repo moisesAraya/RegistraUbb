4a61504251cec2395fdc008019dc8585eecd18c9
4ndreams <andrea.tapia2101@alumnos.ubiobio.cl>
2025-11-15 00:52:11 -0300

    start gunicron

--- lector/start_gunicorn.sh
+++ lector/start_gunicorn.sh
@@ -11,10 +11,5 @@ python manage.py collectstatic --noinput
 echo "🗃️ Ejecutando migraciones..."
 python manage.py migrate --noinput
 
-echo "🚀 Iniciando Gunicorn..."
-# El "exec" reemplaza el proceso actual y mantiene Gunicorn en primer plano
-exec gunicorn mysite.wsgi:application \
-  --bind 0.0.0.0:8000 \
-  --workers 3 \
-  --access-logfile /root/RegistraUbb/lector/gunicorn_access.log \
-  --error-logfile /root/RegistraUbb/lector/gunicorn_error.log
+echo "🚀 Iniciando servidor Django..."
+exec python manage.py runserver 0.0.0.0:8000
